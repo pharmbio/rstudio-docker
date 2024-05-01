738ff0754b5b9ebb4622f734203ee568976f276d
andersla <anders@ormbunkar.se>
2024-05-01 21:35:03 +0200

    upgraded to version 4.3.3

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rocker/rstudio:4.1.1
+FROM rocker/rstudio:4.3.3
 RUN apt-get -y update && apt-get install -y \
     default-jdk \
     && apt-get clean 
--- build-push-docker.sh
+++ build-push-docker.sh
@@ -1,10 +1,2 @@
-
-if [[ $# -eq 0 ]] ; then
-    echo 'You need to specify version as argument, e.g. build-push-docker.sh 4.1.1'
-    exit 0
-fi
-
-VERSION=$1
-
-docker build -t "ghcr.io/pharmbio/rstudio:$VERSION" .
-docker push "ghcr.io/pharmbio/rstudio:$VERSION"
+docker build -t "ghcr.io/pharmbio/rstudio:4.3.3" .
+docker push "ghcr.io/pharmbio/rstudio:4.3.3"
